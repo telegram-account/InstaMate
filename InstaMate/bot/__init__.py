e81682e33a54377c1c6d48397f0c2e0b77e112d8
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-30 02:24:59 +0530

    Update __init__.py

--- InstaMate/bot/__init__.py
+++ InstaMate/bot/__init__.py
@@ -1,3 +1,6 @@
+# fileName: nabilanavab/instaMate/bot/__init__.py
+# copyright ©️ 2023 nabilanavab
+
 import telebot
 from .util import config
 from telebot import types
@@ -9,3 +12,5 @@ class BotInfo:
     bot_id = _bot.id
     bot_first_name = _bot.first_name
     bot_username = _bot.username
+
+# Author : @nabilanavab
